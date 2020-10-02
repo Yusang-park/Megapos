2183cf279f80da5116756fa054fa2649be3d1b4a
milkness <dfhilder@gmail.com>
2020-10-02 23:25:26 +0900

    ver1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
-import 'dart:isolate';
 
-import 'package:capstone/Screen/BasketScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:nfc_in_flutter/nfc_in_flutter.dart';
-
+import 'package:flutter_nfc_plugin/models/nfc_event.dart';
+import 'package:flutter_nfc_plugin/models/nfc_message.dart';
+import 'package:flutter_nfc_plugin/models/nfc_state.dart';
+import 'package:flutter_nfc_plugin/nfc_plugin.dart';
 import 'Screen/HomeScreen.dart';
 
 void main() {
@@ -14,41 +14,25 @@ void main() {
       SystemUiOverlayStyle(statusBarColor: Color(0xFFF9F9F9)));
   runApp(MyApp());
 }
-//hi
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.black, //기본컬러이며 위젯들이 보색을 띄게 된다.
-        textTheme: TextTheme(
-          title: TextStyle(
-              color: Colors.deepPurple,
-              fontFamily: 'Jalnan',
-              fontWeight: FontWeight.bold),
-        ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        buttonTheme: ButtonThemeData(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-          buttonColor: Colors.deepPurple,
-          textTheme: ButtonTextTheme.primary,
-        ),
-      ),
-      home: MyHomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+class _MyAppState extends State<MyApp> {
+  //매장명 수신을 위한 NFC 변수
+  String nfcState = 'Unknown';
+  String nfcError = '';
+  String nfcMessage = '';
+  String nfcTechList = '';
+  String nfcId = '';
+//=============================
+  NfcMessage nfcMessageStartedWith;
 
-class _MyHomePageState extends State<MyHomePage> {
+  NfcPlugin nfcPlugin = NfcPlugin();
+  StreamSubscription<NfcEvent> _nfcMesageSubscription;
+
   //파이어베이스 설정을 위한 변수
   bool _initialized = false;
   bool _error = false;
@@ -69,15 +53,81 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  @override
-  void initState() {
-    initializeFlutterFire(); //파이어베이스 초기화
+//NFC 태그 실행을 위한 함수
+  Future<void> initPlatformState() async {
+    NfcState _nfcState;
 
-    super.initState();
+    try {
+      _nfcState = await nfcPlugin.nfcState;
+      print('NFC state is $_nfcState');
+    } on PlatformException {
+      print('Method "NFC state" exception was thrown');
+    }
+    try {
+      final NfcEvent _nfcEventStartedWith = await nfcPlugin.nfcStartedWith;
+      print('NFC event started with is ${_nfcEventStartedWith.toString()}');
+      if (_nfcEventStartedWith != null) {
+        setState(() {
+          nfcMessageStartedWith = _nfcEventStartedWith.message;
+        });
+      }
+    } on PlatformException {
+      print('Method "NFC event started with" exception was thrown');
+    }
+
+    if (_nfcState == NfcState.enabled) {
+      _nfcMesageSubscription = nfcPlugin.onNfcMessage.listen((NfcEvent event) {
+        if (event.error.isNotEmpty) {
+          setState(() {
+            nfcMessage = 'ERROR: ${event.error}';
+            nfcId = '';
+          });
+        } else {
+          setState(() {
+            nfcMessage = event.message.payload.toString();
+            nfcTechList = event.message.techList.toString();
+            nfcId = event.message.id;
+          });
+        }
+      });
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      nfcState = _nfcState.toString();
+    });
   }
 
   @override
+  void initState() {
+    initializeFlutterFire(); //파이어베이스 초기화
+    initPlatformState();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.black, //기본컬러이며 위젯들이 보색을 띄게 된다.
+          textTheme: TextTheme(
+            title: TextStyle(
+                color: Colors.deepPurple,
+                fontFamily: 'Jalnan',
+                fontWeight: FontWeight.bold),
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          buttonTheme: ButtonThemeData(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+            buttonColor: Colors.deepPurple,
+            textTheme: ButtonTextTheme.primary,
+          ),
+        ),
+        home: HomeScreen());
   }
 }
